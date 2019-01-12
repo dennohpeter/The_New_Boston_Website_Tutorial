4b2cd2442336b46b8a54ab61fafcea86f7d97f6c
El_Professor <denispeterson96@gmail.com>
2019-01-12 23:02:07 +0300

    Changed to render

--- music/urls.py
+++ music/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name="index"),
+    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name="detail"),
 ]
--- music/views.py
+++ music/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Album, Song
 
 
 def index(request):
-    return HttpResponse("<h1>This is the Music App Homepage</h1>")
+    all_albums = Album.objects.all()
+    context = {'all_albums': all_albums}
+    return render(request, 'music/index.html', context)
+
+
+def detail(request, album_id):
+    return HttpResponse("<h2>Details For Album id: " + str(album_id) + "</h2>")
