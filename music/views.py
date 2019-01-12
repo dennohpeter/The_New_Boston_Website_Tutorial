65076fe4570a88f477419aa2cf4eeaa4195144ed
El_Professor <denispeterson96@gmail.com>
2019-01-12 23:33:27 +0300

    Added details page & captured doesnot exist error

--- music/views.py
+++ music/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import Http404
 from .models import Album, Song
 
 
 def index(request):
     all_albums = Album.objects.all()
-    context = {'all_albums': all_albums}
-    return render(request, 'music/index.html', context)
+    return render(request, 'music/index.html', {'all_albums': all_albums})
 
 
 def detail(request, album_id):
-    return HttpResponse("<h2>Details For Album id: " + str(album_id) + "</h2>")
+    try:
+        album = Album.objects.get(pk=album_id)
+    except Album.DoesNotExist:
+        raise Http404("That Album Does Not Exist")
+    return render(request, 'music/detail.html', {'album': album})
